6adab4bdd23764038f2173588ab2ca500ebe9768
Marcos Hack <marcoshack@gmail.com>
2012-07-06 00:54:18 -0300

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,7 @@ class Product
   include Mongoid::Document
   field :t, as: :title, :type => String
   field :d, as: :description, :type => String
+  
+  validates :title, presence: true, length: { maximum: 100 }
 end
-
-# p = Product.new(title: "Samsung Galaxy S")
-# p.attributes #=> { "t" => "Samsung Galaxy S"}
-# p.title      #=> "Samsung Galaxy S"
-
 
